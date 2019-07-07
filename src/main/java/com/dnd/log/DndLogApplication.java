b909aac6d9f41e61a721928f71ff068bde3c5cf9
andy <acongos92@gmail.com>
2019-07-07 10:55:15 -0400

    remove unused import and implementation

--- src/main/java/com/dnd/log/DndLogApplication.java
+++ src/main/java/com/dnd/log/DndLogApplication.java
@@ -1,23 +1,14 @@
 package com.dnd.log;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BatchPreparedStatementSetter;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.dnd.log.player.Player;
 
 @SpringBootApplication
-public class DndLogApplication implements CommandLineRunner{
+public class DndLogApplication{
 	
 	public static final Logger logger = LoggerFactory.getLogger(DndLogApplication.class);
 	
