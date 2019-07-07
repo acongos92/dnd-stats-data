374290f2ec7259a2b8e79b82a326bab56b841307
andy <acongos92@gmail.com>
2019-07-07 15:56:36 -0400

    remove boilerplate reference

--- src/main/java/com/dnd/log/DndLogApplication.java
+++ src/main/java/com/dnd/log/DndLogApplication.java
@@ -16,7 +16,7 @@ import com.dnd.log.player.Player;
 
 
 @SpringBootApplication
-public class DndLogApplication implements CommandLineRunner{
+public class DndLogApplication {
 	
 	public static final Logger logger = LoggerFactory.getLogger(DndLogApplication.class);
 	
@@ -28,22 +28,7 @@ public class DndLogApplication implements CommandLineRunner{
 	}
 
 
-	@Value("${spring.datasource.url}")
-	private String dbName;
-	
-	@Autowired
-	private JdbcTemplate jdbc;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		logger.info("DB NAME: " + dbName);
-		List<Player> players = jdbc.queryForList("SELECT * FROM PLAYER", Player.class);
-		for(Player player: players) {
-			logger.info("PLAYER: " + player.getName());
-		}
-		
-	}
+
 	
 
 }
